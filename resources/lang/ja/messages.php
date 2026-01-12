21c719c678710a057dd464fa07171ab850dc114b
Lee Torres <grafrafraftorres28@gmail.com>
2026-01-12 19:36:00 +0800

    Create messages.php

---
+++ resources/lang/ja/messages.php
@@ -0,0 +1,51 @@
+<?php
+
+return [
+    // Landing Page
+    'site_title' => 'ランダムエイゴ - オンライン英会話レッスン',
+    'site_subtitle' => '幼児・小学生・中高生まで子供オンライン英会話・英語学習のSレッスン',
+    'hero_title' => '子供専用',
+    'hero_subtitle' => 'オンライン英会話',
+    'target_age' => '対象年齢',
+    'age_range' => '3才～小学生・中学生・高校生',
+    
+    // Features
+    'feature_1_title' => 'リーズナブル',
+    'feature_1_desc' => '1レッスン 22分<br>1,000~2,500円',
+    'feature_2_title' => '送迎不要',
+    'feature_2_desc' => 'スケジュールを柔軟に組める',
+    'feature_3_title' => '話したくなる',
+    'feature_3_desc' => '1対1で楽しくアウトプット',
+    
+    // CTA
+    'cta_title' => '開始前にカウンセラーに相談できる！',
+    'cta_button' => '無料体験レッスンを申し込む',
+    
+    // Navigation
+    'nav_home' => 'ホーム',
+    'nav_about' => 'Sレッスンについて',
+    'nav_instructors' => '講師',
+    'nav_pricing' => '料金プラン',
+    'nav_trial' => '体験談',
+    'nav_contact' => 'お問い合わせ',
+    
+    // Auth
+    'login' => 'ログイン',
+    'register' => '新規登録',
+    'logout' => 'ログアウト',
+    'email' => 'メールアドレス',
+    'password' => 'パスワード',
+    'password_confirmation' => 'パスワード確認',
+    'name' => '名前',
+    'remember_me' => '次回から自動でログインする',
+    'forgot_password' => 'パスワードをお忘れですか？',
+    'no_account' => 'アカウントをお持ちでないですか？',
+    'have_account' => 'すでにアカウントをお持ちですか？',
+    'login_title' => 'ログイン',
+    'register_title' => 'アカウント作成',
+    
+    // Common
+    'teacher_login' => '講師専用ログイン',
+    'member_login' => '会員専用ログイン',
+    'free_trial' => '無料体験レッスン',
+];
