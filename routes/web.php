76ebf19507e1fcefbf5fc7a7e15d33baf8a02970
Lee Torres <grafrafraftorres28@gmail.com>
2026-01-12 19:36:13 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\LanguageController;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
+// Language switcher
+Route::get('/lang/{locale}', [LanguageController::class, 'switch'])->name('lang.switch');
+
+// Authentication routes
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
