d1a5ffdcd7d3abf235485d5e6621e11df2a49052
Lee Torres <grafrafraftorres28@gmail.com>
2026-01-16 20:20:20 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,7 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+// Include role-specific routes
+require __DIR__.'/student.php';
+require __DIR__.'/admin.php';
