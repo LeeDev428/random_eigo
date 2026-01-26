d4de51a7938f8b456768fb6260b6841acf4ebd09
Lee Torres <grafrafraftorres28@gmail.com>
2026-01-27 02:46:28 +0800

    Create Payment.php

---
+++ app/Models/Payment.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'student_id',
+        'course_id',
+        'amount',
+        'payment_method',
+        'description',
+        'status',
+        'payment_date',
+    ];
+
+    protected $casts = [
+        'payment_date' => 'date',
+    ];
+
+    public function student()
+    {
+        return $this->belongsTo(User::class, 'student_id');
+    }
+
+    public function course()
+    {
+        return $this->belongsTo(Course::class);
+    }
+}
